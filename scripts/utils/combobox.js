290c81bc7ea948190589900709559869ad1b8bc9
Gwenaëlle Herissant <gh@pointligneplan.fr>
2022-09-15 12:03:53 +0200

    [fix] combobox not updating after erasing search

--- scripts/utils/combobox.js
+++ scripts/utils/combobox.js
@@ -49,6 +49,10 @@ function openCombobox(element, filteredRecipes) {
 
   createDataList(filteredRecipes, element);
 
+  if (search.value.length === 0) {
+    createDataList(recipes, element);
+  }
+
   document.querySelectorAll('.combobox .list span').forEach(span => {
     span.addEventListener('click', (e) => {
       addTag(e.target);
